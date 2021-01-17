4d11a8dac7040ea66b1556893542f6bae1ed0507
Agustin Biancardi <agus@pop-os.localdomain>
2021-01-16 20:56:17 -0500

    Final commit, cleaner code and prevented large memory leaks :)

--- PaintDryTimerStarter.cpp
+++ PaintDryTimerStarter.cpp
@@ -18,23 +18,18 @@ struct DryingSnapShot {
 	TimeCode *timeToDry; //this pointer points to a TimeCode
 };
 
+// gets time till drying is complete for each DSS struct being referenced
 long long int get_time_remaining(DryingSnapShot dss){
 	size_t start = dss.startTime;
 	size_t end = time(0);
 
 	long long timeElapsed = end - start; // how much time has passed
 	long long timeRemaining = dss.timeToDry->GetTimeCodeAsSeconds() - timeElapsed;
-	// cout << "TIME TO DRY " << timeToDry << endl;
-	// cout << "START " << start << endl;
-	// cout << "END " << end << endl;
-	// cout << "TIME ELAPSED " << timeElapsed << endl;
-	// cout << "TIME REMAINING " << timeRemaining << endl << endl;
+
 	return timeRemaining;
 }
 
-// What is my purpose? 
-// You print a string with relevant DryingSnapShot info!
-// Oh... my god
+//prints DryingSnapShot with relevant information
 string drying_snap_shot_to_string(DryingSnapShot dss){
 	long long int time_remaining = get_time_remaining(dss);
 	string string_time = to_string(time_remaining);
@@ -42,113 +37,126 @@ string drying_snap_shot_to_string(DryingSnapShot dss){
 	return "BATCH-" + dss.name + "(takes " + t.ToString() + " to dry) ";
 }
 
+// Uses surface area formula to (4*pi*r^2) to calculate seconds required
 double get_sphere_sa(double rad){
 	double surface_area = 4 * M_PI * rad * rad;
 	return surface_area;
 }
 
+//given a surface area, will create a TimeCode pointer that points to a TimeCode set at the Surface Area
 TimeCode *compute_time_code(double surfaceArea){
 	TimeCode *t = new TimeCode(0,0,surfaceArea); 
 	return t;
 }
+void tests(){
+	// get_time_remaining
+	DryingSnapShot dss;
+	dss.startTime = time(0);
+	TimeCode tc = TimeCode(0, 0, 7);
+	dss.timeToDry = &tc;
+	long long int ans = get_time_remaining(dss);
+	assert(ans > 6 && ans < 8);
 
+	DryingSnapShot dss2;
+	dss2.startTime = time(0);
+	tc = TimeCode(0, 0, 15);
+	dss2.timeToDry = &tc;
+	ans = get_time_remaining(dss);
+	assert(ans > 14 && ans < 16);
+
+	DryingSnapShot dss3;
+	dss3.startTime = time(0);
+	tc = TimeCode(0, 1, 15);
+	dss3.timeToDry = &tc;
+	ans = get_time_remaining(dss);
+	assert(ans > 74 && ans < 76);
+
+	// get_sphere_sa
+	double sa = get_sphere_sa(2.0);
+	assert (50.2654 < sa && sa < 50.2655);
+
+	// compute_time_code
+	TimeCode *tc2 = compute_time_code(1.0);
+	cout << "tc: " << tc.GetTimeCodeAsSeconds() << endl;
+	assert(tc2->GetTimeCodeAsSeconds() == 1);
+	delete tc2;
+
+	cout << "ALL TESTS PASSED!" << endl;
+}	
 int main(){
 	
-	vector<DryingSnapShot> dssList;
-	vector<DryingSnapShot> cleaner; //used to help arrange wet items
+	//tests();
+	vector<DryingSnapShot> tracker;
+	vector<DryingSnapShot> cleaner; //used to help arrange wet items, very challenging to do with just the tracker vector
 
 	cout << "Choose an option: (A)dd, (V)iew Current Items, (Q)uit: ";
 	
 	string userResponse = "p";
 	while(userResponse != "q"){
+	
 	// at the beginning of every loop, check if any of the batches have finished, remove them from BatchVectors
-		
-		for (size_t i = 0; i < dssList.size(); i++){
-			DryingSnapShot curr = dssList.at(i);
+		for (size_t i = 0; i < tracker.size(); i++){
+			DryingSnapShot curr = tracker.at(i);
+			// if DSS has finished drying,place it in cleaner vector
 			if (get_time_remaining(curr) > 0){
 				cleaner.push_back(curr);
 			}
-			// delete curr.timeToDry;
-			// 	dssList.erase(dssList.begin()+i);
-			// 	i++;
-			// }
 		}
-		dssList.clear();
-		dssList = cleaner;
+		//Delete everything in original tracker to prevent memory leaks
+		for(size_t i = 0; i < tracker.size(); i++){
+			tracker.at(i).timeToDry = nullptr;
+			delete tracker.at(i).timeToDry;	
+		}
+		tracker.clear(); //reset tracker
+		tracker = cleaner; // assign al cleaner objects to tracker
+		
+		//delete everything in cleaner and reset cleaner vector for further use
+		for(size_t i = 0; i < cleaner.size(); i++){
+			cleaner.at(i).timeToDry = nullptr;
+			delete cleaner.at(i).timeToDry;	
+		}
 		cleaner.clear();
+
+		//if user wishes to view current batches, print all batches available in tracker vector...
 		if(userResponse == "v"){
-			for (size_t i = 0; i < dssList.size(); i++){
-				DryingSnapShot curr = dssList.at(i);		
+			for (size_t i = 0; i < tracker.size(); i++){
+				DryingSnapShot curr = tracker.at(i);		
 				cout << drying_snap_shot_to_string(curr) << endl;
 			}
-			cout << dssList.size() << " items being tracked.";
+			cout << tracker.size() << " items being tracked.";
 		}
 		if(userResponse == "a"){
-			int randID = rand();
-
-			int radius = (rand() % 40) * 0.25; // set to 7  for testing purposes ie: till I squash this damn bug
+			int randID = rand(); // random name id generated here for each batch
+			int radius = (rand() % 40) * 0.25; // selecting random radii for adding
 			cout << "radius: " << radius << endl;
 
-			DryingSnapShot tss; // create a Drying SnapShot called tss
-			tss.name = to_string(randID); // give it identifier
-			tss.startTime = time(0); //set its start time
+			//if user wants to create new DSS...
+			DryingSnapShot tss; 			// create a Drying SnapShot called tss
+			tss.name = to_string(randID);   // give it identifier
+			tss.startTime = time(0);        //set its start time
 
-			// TimeCode tc = *compute_time_code(get_sphere_sa(rad));
-			tss.timeToDry = new TimeCode(0,0,radius);
-
+			tss.timeToDry = compute_time_code(get_sphere_sa(radius)); // creates TimeCode with expected drying duration
 			cout << tss.timeToDry->ToString() << endl;
-			TimeCode remainingTime = TimeCode(0,0,get_time_remaining(tss));
+
+			TimeCode remainingTime = TimeCode(0,0,get_time_remaining(tss)); // creates a temp TimeCode to display time remaining in 00:00:00 format
 			cout << "Batch-" << tss.name << "will dry in " << remainingTime.ToString() << endl;
-			dssList.push_back(tss);
+			tracker.push_back(tss); // place DSS in tracker
 		}
 		cin >> userResponse;
 	}
+
+	//when we are done running the program... we have to free the memory remaining on our tracker and helper vectors!!
+	//this took days to discover... never forget
+		for(size_t i = 0; i < tracker.size(); i++){
+			delete tracker.at(i).timeToDry;	
+			tracker.at(i).timeToDry = nullptr;	
+		}	
+		for(size_t i = 0; i < cleaner.size(); i++){
+			delete cleaner.at(i).timeToDry;	
+			cleaner.at(i).timeToDry = nullptr;
+		}
+		cleaner.clear();
+		tracker.clear();
+	
 	return 0;
 }
-
-
-
-	void tests(){
-
-	
-	// // get_time_remaining
-	// DryingSnapShot dss;
-	// dss.startTime = time(0);
-	// TimeCode tc = TimeCode(0, 0, 7);
-	// dss.timeToDry = &tc;
-	// long long int ans = get_time_remaining(dss);
-	// assert(ans > 6 && ans < 8);
-
-	// DryingSnapShot dss2;
-	// dss2.startTime = time(0);
-	// tc = TimeCode(0, 0, 15);
-	// dss2.timeToDry = &tc;
-	// ans = get_time_remaining(dss);
-	// assert(ans > 14 && ans < 16);
-
-	// DryingSnapShot dss3;
-	// dss3.startTime = time(0);
-	// tc = TimeCode(0, 1, 15);
-	// dss3.timeToDry = &tc;
-	// ans = get_time_remaining(dss);
-	// assert(ans > 74 && ans < 76);
-
-	// // add more tests here
-
-
-	// // get_sphere_sa
-	// double sa = get_sphere_sa(2.0);
-	// assert (50.2654 < sa && sa < 50.2655);
-	// // add more tests here
-
-	// // compute_time_code
-	// TimeCode *tc2 = compute_time_code(1.0);
-	// cout << "tc: " << tc.GetTimeCodeAsSeconds() << endl;
-	// assert(tc2->GetTimeCodeAsSeconds() == 1);
-	// delete tc2;
-
-	// add more tests here
-	// cout << "ALL TESTS PASSED!" << endl;
-
-
-	}	
